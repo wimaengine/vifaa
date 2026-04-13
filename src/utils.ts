72a84132f2ad1e3c8cac8317a641a74cd52b999b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-13 12:58:00 +0300

    Add `assert`

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+/**
+ * Throws an error if the supplied test is falsy.
+ */
+export function assert<T>(test: T, message: unknown): asserts test is NonNullable<T> {
+  if (test == void 0) {
+    throw message
+  }
+}

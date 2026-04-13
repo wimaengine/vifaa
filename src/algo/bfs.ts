aa01e350cb8f7234267d1afb4f6704e8fc5e8185
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-13 20:11:00 +0300

    Add breadth first search algorithm

---
+++ src/algo/bfs.ts
@@ -0,0 +1,26 @@
+import type { Graph, NodeId } from '../structs/graphs/graph'
+
+export function bfs<T, U>(graph: Graph<T, U>, start: NodeId, visit: (nodeId: NodeId) => void): void {
+  const nodeCount = graph.getNodeCount()
+  const visited = new Array<boolean>(nodeCount).fill(false)
+  const queue: NodeId[] = []
+
+  visited[start] = true
+  queue.push(start)
+
+  while (queue.length > 0) {
+    const nodeId = queue.shift()
+    if (nodeId === undefined) {
+      break
+    }
+
+    visit(nodeId)
+
+    for (const neighbour of graph.getNeighbours(nodeId)) {
+      if (!visited[neighbour]) {
+        visited[neighbour] = true
+        queue.push(neighbour)
+      }
+    }
+  }
+}

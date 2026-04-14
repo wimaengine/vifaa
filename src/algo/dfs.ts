ee31e17c8f4b3bb1f0138cd367c9573a363b4c4c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-14 08:22:00 +0300

    Add depth first search algorithm

---
+++ src/algo/dfs.ts
@@ -0,0 +1,29 @@
+import type { Graph, NodeId } from '../structs/graphs/graph'
+
+export function dfs<T, U>(graph: Graph<T, U>, start: NodeId, visit: (nodeId: NodeId) => void): void {
+  const nodeCount = graph.getNodeCount()
+  const visited = new Array<boolean>(nodeCount).fill(false)
+  const stack: NodeId[] = []
+
+  stack.push(start)
+
+  while (stack.length > 0) {
+    const nodeId = stack.pop()
+    if (nodeId === undefined) {
+      break
+    }
+
+    if (visited[nodeId]) {
+      continue
+    }
+
+    visited[nodeId] = true
+    visit(nodeId)
+
+    for (const neighbour of graph.getNeighbours(nodeId)) {
+      if (!visited[neighbour]) {
+        stack.push(neighbour)
+      }
+    }
+  }
+}

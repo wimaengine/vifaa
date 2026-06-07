4011ba9ee1a9cd33f16b14872caaa62c4eab6925
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-07 11:05:24 +0300

    Add serialization/deserialization methods to `Range`

--- src/structs/range.ts
+++ src/structs/range.ts
@@ -1,9 +1,18 @@
+export type RangeSerial = {
+  start: number
+  end: number
+}
+
 export class Range {
   start: number
-  end:number
+  end: number
   constructor(start = 0, end = 1) {
     this.start = start
     this.end = end
+  }
+
+  serialize() {
+    return Range.serialize(this)
   }
 
   valid() {
@@ -12,5 +21,33 @@ export class Range {
 
   lerp(t: number) {
     return this.start + t * (this.end - this.start)
+  }
+
+  /**
+   * @param {unknown} value
+   */
+  static serialize(value: Range) {
+    return {
+      start: value.start,
+      end: value.end
+    }
+  }
+
+  static validSerial(value: unknown): value is RangeSerial {
+    return !!value
+      && typeof value === 'object'
+      && typeof (value as RangeSerial).start === 'number'
+      && typeof (value as RangeSerial).end === 'number'
+  }
+
+  /**
+   * @param {RangeSerial} value
+   * @param {Range} [out]
+   */
+  static deserialize(value: RangeSerial, out = new Range()) {
+    out.start = value.start
+    out.end = value.end
+
+    return out
   }
 }

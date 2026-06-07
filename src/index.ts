c0a6dc07faabfff30367d0e1c51242fe1ae96a5d
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-07 09:38:09 +0300

    Update tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './algo'
 export * from './structs'
 export * from './types'
+export * from './utils'
--- tests/structs/denselist.test.js
+++ tests/structs/denselist.test.js
@@ -1,24 +1,24 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 
-import { DenseList } from '../../dist/index.module.js'
+import { DenseList, IndexAllocator } from '../../dist/index.module.js'
 
 test('DenseList push returns a numeric index', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   const id = list.push('first')
 
   assert.equal(typeof id, 'number')
 })
 
 test('DenseList get returns value at pushed index', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   const id = list.push('first')
 
   assert.equal(list.get(id), 'first')
 })
 
 test('DenseList set updates value at an allocated index', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   const id = list.push('first')
 
   list.set(id, 'updated')
@@ -27,13 +27,13 @@ test('DenseList set updates value at an allocated index', () => {
 })
 
 test('DenseList reserve returns a numeric index', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
 
   assert.equal(typeof list.reserve(), 'number')
 })
 
 test('DenseList recycle makes an index available again', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   const id = list.reserve()
 
   list.recycle(id)
@@ -42,7 +42,7 @@ test('DenseList recycle makes an index available again', () => {
 })
 
 test('DenseList values includes pushed elements', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   list.push('A')
   list.push('B')
 
@@ -50,13 +50,13 @@ test('DenseList values includes pushed elements', () => {
 })
 
 test('DenseList set on never-allocated index throws', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
 
   assert.throws(() => list.set(1, 'x'), /never been allocated/)
 })
 
 test('DenseList does not write value when set throws on never-allocated index', () => {
-  const list = new DenseList()
+  const list = new DenseList(new IndexAllocator())
   assert.throws(() => list.set(1, 'x'), /never been allocated/)
 
   assert.equal(list.get(1), undefined)

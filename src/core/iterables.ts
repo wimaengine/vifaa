f20f6a4030c01667ee3a4aaef6e810ac2d4501bb
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-25 20:48:12 +0300

    Rename `EdgeIterable` to `NodeEdgeIterable`

--- src/core/iterables.ts
+++ src/core/iterables.ts
@@ -6,6 +6,6 @@ export interface NodeIterable<NodeId extends number = number> {
   forEachNode(callback: (nodeId: NodeId) => void): void
 }
 
-export interface EdgeIterable<NodeId extends number = number, EdgeId extends number = number> {
-  forEachEdge(id: NodeId, callback: (edgeId: EdgeId) => void): void
+export interface NodeEdgeIterable<NodeId extends number = number, EdgeId extends number = number> {
+  forEachNodeEdge(id: NodeId, callback: (edgeId: EdgeId) => void): void
 }

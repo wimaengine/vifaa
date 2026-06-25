6188698a3f8e8621d72023eb4374052d1d2d345f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-25 20:44:21 +0300

    Add `NodeItarable` interface

--- src/core/iterables.ts
+++ src/core/iterables.ts
@@ -2,6 +2,10 @@ export interface NeighbourIterable<NodeId extends number = number> {
   forEachNeighbour(id: NodeId, callback: (nodeId: NodeId) => void): void
 }
 
+export interface NodeIterable<NodeId extends number = number> {
+  forEachNode(callback: (nodeId: NodeId) => void): void
+}
+
 export interface EdgeIterable<NodeId extends number = number, EdgeId extends number = number> {
   forEachEdge(id: NodeId, callback: (edgeId: EdgeId) => void): void
 }

b789cec9f7f29bafaa11ab72ff41caf7df6a812b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-25 20:53:21 +0300

    Remove `NodeAccessor.getNodes` and `EdgeAccessor.getEdges`

--- src/core/accessors.ts
+++ src/core/accessors.ts
@@ -1,11 +1,9 @@
 export interface NodeAccessor<Node, NodeId extends number = number> {
   getNode(id: NodeId): Node | undefined
-  getNodes(): Iterable<Node>
   getNodeCount(): number
 }
 
 export interface EdgeAccessor<Edge, EdgeId extends number = number> {
   getEdge(id: EdgeId): Edge | undefined
-  getEdges(): Iterable<Edge>
   getEdgeCount(): number
 }
